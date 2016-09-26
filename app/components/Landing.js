0ece86f8951001f7837bae484155825d37f4c561
benlayer <ben@tuimedia.com>
2016-09-26 16:04:53 +0100

    Flesh out the landing screen

--- app/components/Landing.js
+++ app/components/Landing.js
@@ -2,15 +2,18 @@
 
 import React, { Component } from 'react';
 import {
+  Image,
   Text,
   TouchableOpacity,
   View,
 } from 'react-native';
 
 import Icon from 'react-native-vector-icons/Ionicons';
-import NavigationBar from 'react-native-navbar';
+
 import Colors from '../styles/colors';
-import { globals } from '../styles';
+import { globals, landingStyles } from '../styles';
+
+const styles = landingStyles;
 
 class Landing extends Component {
   constructor() {
@@ -28,19 +31,34 @@ class Landing extends Component {
     let titleConfig = { title: 'Landing', tintColor: 'white' };
 
     return (
-      <View style={globals.flexContainer}>
-        <NavigationBar
-          title={titleConfig}
-          tintColor={Colors.brandPrimary}
-        />
-        <View style={globals.flexCenter}>
-          <Text style={globals.h2}>
-            This is the Landing Page
-          </Text>
-          <TouchableOpacity onPress={this.visitDashboard}>
-            <Text>Go to the Dashboard</Text>
-          </TouchableOpacity>
+      <View style={styles.container}>
+        <View style={styles.container}>
+          <Image
+            style={styles.backgroundImage}
+            source={require('../assets/images/welcome@2x.png')}
+          />
         </View>
+        <View style={globals.flexCenter}>
+          <Image
+            style={styles.logo}
+            source={require('../assets/images/logo.png')}
+          />
+          <Text style={[globals.lightText, globals.h2, globals.mb2]}>
+            assemblies
+          </Text>
+          <Text style={[globals.lightText, globals.h4]}>
+            Where Developers Connect
+          </Text>
+        </View>
+        <TouchableOpacity
+          style={globals.button}
+          onPress={this.visitDashboard}
+        >
+          <Icon name='ios-person' size={36} color='white' />
+          <Text style={globals.buttonText}>
+            Go to Dashboard
+          </Text>
+        </TouchableOpacity>
       </View>
     );
   }
